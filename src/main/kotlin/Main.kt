924b090885e777571e61ec25e52f5b7b41706456
FawziaAlushrafii <Fawzia.Hassan.ruh.k@tuwaiq.edu.sa>
2021-10-20 12:54:29 +0300

    file renane

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,19 @@
+import kotlin.random.Random
+//E5.7 pag75
 fun main() {
-    println("Designs a caluator")
-    println("Enter two value")
-    print("a =")
-    var a = readLine()!!.toDouble()
-    print("b =")
-    var b = readLine()!!.toDouble()
-    println("1.Addiion \n2. Substraction \n3. Multiplication \n4. Divide \n5.Modulo ")
-    print("Enter chice:")
-    var f = readLine()!!.toInt()
-    print("result :")
-    when (f) {
-        1 -> print("a+b=${a + b}")
-        2 -> print("a-b=${a - b}")
-        3 -> print("a*b=${a * b}")
-        4 -> print("a/b=${a / b}")
-        5 -> print("a%b=${a % b}")
-        else -> print("Wrong choice")
+        val greetingFunction = { playerName: String, numBuildings: Int ->
+            val currentYear = 2018
+            println("Adding $numBuildings houses")
+            "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+        }
+        println(greetingFunction("Guyal", 2))
+        runSimulation("Guyal", greetingFunction)
     }
-}
+    fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+        val numBuildings = Random.nextInt(1,3 )
+         // Randomly selects 1, 2, or 3
+        println(greetingFunction(playerName, numBuildings))
+    }
+
+//E5.9
+
